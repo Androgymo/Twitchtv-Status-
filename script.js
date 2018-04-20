4de1d58a5d461cf35c032e3d62c826f223d18e0f
Androgymo <androgymo@gmail.com>
2018-04-20 13:57:02 -0400

    added twitch api code and additional styling

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 $(function(){
-	$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-  console.log(data);
-});
+	function myRequest(url, tipoRequest) {
+		return new Promise((resolve) => {
+			var rawData, data;
+			var request = new XMLHttpRequest();
+			request.open('GET', url, true);
+			request.onload = function() {
+				if (this.status >= 200 && this.status < 400) {
+					rawData = this.response;
+					data = JSON.parse(this.response);
+					resolve(data);
+				}
+			}
+			request.send();
+		})
+	}
+	
+	function misLlamadas() {
+		var channel1 = 'castro_1021';
+		var channel2 = 'monstercat';
+		var channel = channel2;
+		var clientId = 'c8a3wkkb56yqjhlcui7tcfyjvs65dy6';
+		var url = 'https://api.twitch.tv/kraken/streams/' + channel + '?client_id=' + clientId;
+		var tipoRequest = '';
+
+		myRequest(url, tipoRequest).then((data) => {
+			console.log(data);
+		});
+	} 
+	misLlamadas();
 });
